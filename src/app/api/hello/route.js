f3207c9fa3326dc4413cfbbc2bfcf8e439f84560
nicoconti1901 <nico.conti.1901@gmail.com>
2023-09-19 12:23:53 -0300

    conexion con la base de datos terminada

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
+import {conn} from "@/libs/mysql";
 
-export function GET() {
+export async function GET() {
+  const results = await conn.query("SELECT NOW()");
+  console.log(results);
   return NextResponse.json("Hello World");
 }
 
